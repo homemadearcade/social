14ba3a4ba0394bbdc9e8659e8a12b4bbe40156f9
Jonathan Pedigo <jon.pedigo@feedly.com>
2020-09-30 15:51:04 -0500

    client prefix

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(express.static("public"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client/public", "index.html"));
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
