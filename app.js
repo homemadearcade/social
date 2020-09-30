efa45274936a4483fe70e1f54d1bdeb8890be21e
Jonathan Pedigo <jon.pedigo@feedly.com>
2020-09-30 16:15:33 -0500

    can I get images

--- app.js
+++ app.js
@@ -102,6 +102,8 @@ if (process.env.NODE_ENV === "production") {
 } else {
   app.use(logger("dev"));
 }
+
+app.use(express.static("public"));
 app.use(express.static("client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
