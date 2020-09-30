fe956a367679970934e9d5fd286f8334ba988f24
Jonathan Pedigo <jon.pedigo@feedly.com>
2020-09-30 14:25:59 -0500

    dont use local up

--- client/src/_services/socketService.js
+++ client/src/_services/socketService.js
@@ -6,7 +6,7 @@ export const socketService = {
 //window.location.hostname
 function connect() {
   return new Promise((resolve, reject) => {
-    const socket = io('192.168.0.14:5000', {
+    const socket = io(window.location.hostname, {
       query: { token: JSON.parse(localStorage.getItem("user")).token },
     });
     socket.on("connect", () => {
